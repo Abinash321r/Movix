581f0eb58a00bbc6c90834190441a9d2c981fe35
Abinash321r <abinashnepali619@gmail.com>
2026-02-06 22:50:56 +0545

    updated css

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE_URL);
+    await mongoose.connect(process.env.DATABASE_URL,{ family: 4 });
     console.log("MongoDB Connected Successfully 🚀");
   } catch (error) {
     console.error("MongoDB Connection Error ❌", error.message);
